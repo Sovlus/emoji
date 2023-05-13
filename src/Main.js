dd21f0638a59c4a8fff338ab11bb3760ce1735bd
Sovlus <kubbaz122@gmail.com>
2023-05-13 14:32:06 +0200

    create main function

--- src/Main.js
+++ src/Main.js
@@ -3,3 +3,32 @@ import { Picker } from 'emoji-mart';
 import 'emoji-mart/css/emoji-mart.css';
 import './App.css';
 
+function Main() {
+    const [search, SetSearchValue] = useState('');
+
+    const zmianaWyszukiwania = (event) => {
+        SetSearchValue(event.target.value);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Main;
