7b695e680269eeb6e04d7d043142a9d786a37626
Sovlus <kubbaz122@gmail.com>
2023-05-13 14:36:06 +0200

    finished

--- src/Main.js
+++ src/Main.js
@@ -8,17 +8,22 @@ function Main() {
 
     const zmianaWyszukiwania = (event) => {
         SetSearchValue(event.target.value);
-    }
-};
+    };
 
     return (
         <div className='Main'>
         <h1>Wyszukaj Emoji</h1>
         <input type="text" placeholder='wpisz coś' value={search} onChange={zmianaWyszukiwania} />
+        
         <div className="choose">
             <Picker set="apple" style={{ width: '100%', height: '100%' }} />
         </div>
-    )
+        </div>
+
+        
+    );
+}
+
 
 
 
